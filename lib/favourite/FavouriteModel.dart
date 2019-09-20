f3e5e33ddc858bea611eb1e8c8e19f1f3a9e9bf7
Mohit Dholakia <mohit.dholakia@solutionanalysts.com>
2019-09-20 19:14:24 +0530

    favourite list added

---
+++ lib/favourite/FavouriteModel.dart
@@ -0,0 +1,40 @@
+class FavouriteModel {
+    String complexity;
+    String firstName;
+    String lastName;
+    String name;
+    String photo;
+    String preparationTime;
+    int recipeId;
+    String serves;
+
+    FavouriteModel({this.complexity, this.firstName, this.lastName, this.name, this.photo, this.preparationTime, this.recipeId, this.serves});
+
+    factory FavouriteModel.fromJson(Map<String, dynamic> json) {
+        return FavouriteModel(
+            complexity: json['complexity'], 
+            firstName: json['firstName'], 
+            lastName: json['lastName'], 
+            name: json['name'], 
+            photo: json['photo'] != null ? (json['photo']) : null,
+            preparationTime: json['preparationTime'], 
+            recipeId: json['recipeId'], 
+            serves: json['serves'], 
+        );
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['complexity'] = this.complexity;
+        data['firstName'] = this.firstName;
+        data['lastName'] = this.lastName;
+        data['name'] = this.name;
+        data['preparationTime'] = this.preparationTime;
+        data['recipeId'] = this.recipeId;
+        data['serves'] = this.serves;
+        if (this.photo != null) {
+            data['photo'] = this.photo;
+        }
+        return data;
+    }
+}
